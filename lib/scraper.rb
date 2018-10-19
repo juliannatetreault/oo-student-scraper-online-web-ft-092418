aa4ca031cd8f671724d1dee5b990b5186a0db7f3
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-19 19:05:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,13 +6,21 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    binding.pry
+    #binding.pry
+    #doc.css("div.student-card").collect do |student|
+    #student_hash = {
+    #:name => doc.css("h4.student-name").text,
+    #:location => doc.css("p.student-location").text,
+    #:profile_url => doc.css("div.student-card a").text
+    }
+  end
+  #student_hash
+#end
+
+  def self.scrape_profile_page(profile_url)
+    #doc = Nokogiri::HTML(open(profile_url))
+    #binding.pry
     #doc.css()
   end
 
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
 end
-

97ea3cd704971db3087a313d69ae3946ee3f1542
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-19 19:12:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ end
       profile_quote: doc.css(".profile-quote").text
     }
     
-    links = doc.css(".social-icon-container a").collect { |p| p.attribute("href")}
+    links = doc.css(".social-icon-container a").collect { |p| p.attribute("href").value}
   end
 
 end

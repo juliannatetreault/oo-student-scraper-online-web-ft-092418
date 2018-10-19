fbdde6338ace299d31e4fb5b7abb3423a004d2b1
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-19 19:07:53 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
     student_hash = {
     name: student.css("h4.student-name").text,
     location: student.css("p.student-location").text,
-    profile_url: student.css("div.student-card a").text
+    profile_url: student.css("a").attr().valu
     }
   end
   #student_hash
